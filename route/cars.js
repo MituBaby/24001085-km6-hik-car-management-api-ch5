9540cad21a60344533817265d0148d63fad24ca9
Mitu Baby <ucihazhaske@gmail.com>
2024-04-19 14:03:50 +0700

    add role authenticatioin

--- route/cars.js
+++ route/cars.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const carsController = require("../controller/cars");
+const {authMiddleware} = require("../middleware/auth");
 
-router.route("/").get(carsController.getCars).post(carsController.createCars);
+router.route("/").get(authMiddleware(["user", "admin"]), carsController.getCars).post(authMiddleware(["admin"]), carsController.createCars);
 router
   .route("/:id")
-  .get(carsController.getCar)
-  .put(carsController.updateCars)
-  .delete(carsController.deleteCars);
+  .get(authMiddleware(["user", "admin"]), carsController.getCar)
+  .put(authMiddleware(["admin"]), carsController.updateCars)
+  .delete(authMiddleware(["admin"]), carsController.deleteCars);
 
 module.exports = router;
--- route/photoCars.js
+++ route/photoCars.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const photoController = require("../controller/photoCars");
+const {authMiddleware} = require("../middleware/auth");
 
 router
   .route("/")
-  .get(photoController.getPhotos)
-  .post(photoController.createPhoto);
+  .get(authMiddleware(["user", "admin"]), photoController.getPhotos)
+  .post(authMiddleware(["admin"]), photoController.createPhoto);
 
 router
   .route("/:id")
-  .get(photoController.getPhoto)
-  .put(photoController.updatePhoto)
-  .delete(photoController.deletePhoto);
+  .get(authMiddleware(["user", "admin"]), photoController.getPhoto)
+  .put(authMiddleware(["admin"]), photoController.updatePhoto)
+  .delete(authMiddleware(["admin"]), photoController.deletePhoto);
 module.exports = router;
